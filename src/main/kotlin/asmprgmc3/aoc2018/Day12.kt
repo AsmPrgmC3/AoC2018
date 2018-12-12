a09646f7d7b019fb4754165d136a970306905cae
AsmPrgmC3 <asmprgmc3@gmail.com>
2018-12-12 17:09:54 +0100

    Day 12 Part 2

--- src/main/kotlin/asmprgmc3/aoc2018/Day12.kt
+++ src/main/kotlin/asmprgmc3/aoc2018/Day12.kt
@@ -3,7 +3,7 @@ package asmprgmc3.aoc2018
 import java.util.*
 
 object Day12 {
-    fun parseInput(input: List<String>): Pair<LinkedList<Boolean>, List<List<Boolean>>> {
+    private fun parseInput(input: List<String>): Pair<LinkedList<Boolean>, List<List<Boolean>>> {
         val stateRegex = Regex("""initial state: (?<state>[.#]+)""")
         val ruleRegex = Regex("""(?<rule>[.#]{5}) => (?<alive>[.#])""")
 
@@ -25,36 +25,88 @@ object Day12 {
         )
     }
 
-    fun part1(input: List<String>): Int {
+    private fun nextGeneration(offset: Int, rules: List<List<Boolean>>, state: LinkedList<Boolean>): Pair<Int, LinkedList<Boolean>> {
+        var newOffset = offset
+
+        var firstPlant = state.indexOf(true)
+        if (firstPlant < 4) {
+            repeat(4) { state.addFirst(false) }
+            firstPlant += 4
+            newOffset += 4
+        }
+
+        newOffset -= 2
+
+        val lastPlant = state.lastIndexOf(true)
+        if (lastPlant > state.size - 5) {
+            repeat(4) { state.addLast(false) }
+        }
+
+        return Pair(
+                newOffset,
+                state
+                        .asSequence()
+                        .windowed(5)
+                        .map { it in rules }
+                        .toCollection(LinkedList())
+        )
+    }
+
+    private fun calculateSum(offset: Long, state: List<Boolean>): Long {
+        return state
+                .asSequence()
+                .mapIndexed { i, alive -> if (alive) i - offset else 0 }
+                .sum()
+    }
+
+    fun part1(input: List<String>): Long {
         var (state, rules) = parseInput(input)
 
         var offset = 0
 
         repeat(20) { _ ->
-            var firstPlant = state.indexOf(true)
-            if (firstPlant < 4) {
-                repeat(4) { state.addFirst(false) }
-                firstPlant += 4
-                offset += 4
-            }
-
-            val lastPlant = state.lastIndexOf(true)
-            if (lastPlant > state.size - 5) {
-                repeat(4) { state.addLast(false) }
-            }
-
-            state = state
-                    .asSequence()
-                    .windowed(5)
-                    .map { it in rules }
-                    .toCollection(LinkedList())
-
-            offset -= 2
+            val next = nextGeneration(offset, rules, state)
+            offset = next.first
+            state = next.second
         }
+
+        return calculateSum(offset.toLong(), state)
+    }
+
+    private fun getAliveIndices(state: List<Boolean>): List<Int> {
+        val firstAlive = state.indexOf(true)
 
         return state
                 .asSequence()
-                .mapIndexed { i, alive -> if (alive) i - offset else 0 }
-                .sum()
+                .mapIndexed { i, alive -> Pair(i, alive) }
+                .filter { it.second }
+                .map { it.first - firstAlive }
+                .toList()
+    }
+
+    fun part2(input: List<String>): Long {
+        var (state, rules) = parseInput(input)
+
+        var offset = 0
+
+        var indices = getAliveIndices(state)
+        var lastIndices = listOf(0)
+
+        var generations = 0L
+
+        while (indices != lastIndices) {
+            val next = nextGeneration(offset, rules, state)
+            offset = next.first
+            state = next.second
+
+            lastIndices = indices
+            indices = getAliveIndices(state)
+
+            generations++
+        }
+
+        val generationsLeft = 50_000_000_000 - generations
+
+        return calculateSum(offset - generationsLeft, state)
     }
 }
