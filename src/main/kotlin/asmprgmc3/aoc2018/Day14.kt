1824ffc71fe5f4824ba73689aee4e1e9460845f1
AsmPrgmC3 <asmprgmc3@gmail.com>
2018-12-14 10:48:39 +0100

    Day 14 Part 2

--- src/main/kotlin/asmprgmc3/aoc2018/Day14.kt
+++ src/main/kotlin/asmprgmc3/aoc2018/Day14.kt
@@ -1,19 +1,12 @@
 package asmprgmc3.aoc2018
 
 object Day14 {
-    private fun parseInput(input: List<String>): Int {
-        return input[0].toInt()
-    }
-
-    fun part1(input: List<String>): String {
-        val after = parseInput(input)
-        val wait = after + 10
-
+    private fun runRecipes(): Sequence<List<Int>> {
         val recipes = mutableListOf(3, 7)
         var elve1 = 0
         var elve2 = 1
 
-        while (recipes.size < wait) {
+        return generateSequence {
             val score1 = recipes[elve1]
             val score2 = recipes[elve2]
 
@@ -26,8 +19,29 @@ object Day14 {
 
             elve1 = (elve1 + score1 + 1) % recipes.size
             elve2 = (elve2 + score2 + 1) % recipes.size
+
+            recipes
         }
+    }
+
+    fun part1(input: List<String>): String {
+        val after = input[0].toInt()
+        val wait = after + 10
+
+        val recipes = runRecipes().find { it.size >= wait }!!
 
         return (after until after + 10).map { recipes[it] }.joinToString("")
+    }
+
+    fun part2(input: List<String>): Int {
+        val suffix = input[0]
+        val suffixZone = suffix.length + 1
+
+        return runRecipes()
+                .dropWhile { it.size < suffixZone }
+                .map { Pair(it, it.subList(it.size - suffixZone, it.size).joinToString("")) }
+                .filter { it.second.contains(suffix) }
+                .map { it.first.joinToString("").indexOf(suffix) }
+                .first()
     }
 }
